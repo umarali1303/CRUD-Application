fa70a175d6e988dbc0edbeb2a41d707bac6d5c6d
Shahnawaz Shaikh <32239343+IShahnawazShaikh@users.noreply.github.com>
2022-10-09 15:56:17 +0530

    Read API added

--- src/main/java/com/restapp/crudapp/controller/BooksController.java
+++ src/main/java/com/restapp/crudapp/controller/BooksController.java
@@ -20,5 +20,18 @@ public class BooksController {
         return new ResponseEntity(response, HttpStatus.CREATED);
 
     }
+    @GetMapping("/get/{id}")
+    public ResponseEntity getBookById(@PathVariable Long id){
+        var response=bookService.getBookById(id);
+        if(response==null) return new ResponseEntity("Book Not found with the Id "+id,HttpStatus.NOT_FOUND);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/get/all")
+    public ResponseEntity getAllBook(){
+        var response=bookService.getAllBook();
+        return ResponseEntity.ok(response);
+    }
+
 
 }
--- src/main/java/com/restapp/crudapp/service/BookService.java
+++ src/main/java/com/restapp/crudapp/service/BookService.java
@@ -7,6 +7,9 @@ import com.restapp.crudapp.repository.BooksRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class BookService {
 
@@ -24,5 +27,29 @@ public class BookService {
         response.setTitle(bookEntity.getTitle());
         return response;
 
+    }
+
+    public BookResponseDTO getBookById(Long id) {
+        var optionalResponse =booksRepository.findById(id);
+        if(optionalResponse.isPresent()){
+            var responseEntity=optionalResponse.get();
+            BookResponseDTO response=new BookResponseDTO();
+            response.setDescription(responseEntity.getDescription());
+            response.setTitle(responseEntity.getTitle());
+            return response;
+        }
+        return  null;
+    }
+
+    public List<BookResponseDTO> getAllBook() {
+        var responseEntityList=booksRepository.findAll();
+        List<BookResponseDTO> response=new ArrayList<>();
+        for(BookEntity book: responseEntityList){
+            BookResponseDTO dto=new BookResponseDTO();
+            dto.setDescription(book.getDescription());
+            dto.setTitle(book.getTitle());
+            response.add(dto);
+        }
+        return response;
     }
 }
